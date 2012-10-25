55632fa6ed57ae63b54be23c232dc41460f1c637
davidc <davidc@375a5ec8-f457-43dc-b3d4-3e37c4652669>
2012-10-25 16:13:50 +0000

    Finished CrazyIdean, Sucess\!
    git-svn-id: http://192.168.1.2:8080/svn/mcaslite2.0@16 375a5ec8-f457-43dc-b3d4-3e37c4652669

--- manager/trunk/src/cat/i2cat/mcaslite/utils/TranscoderUtils.java
+++ manager/trunk/src/cat/i2cat/mcaslite/utils/TranscoderUtils.java
@@ -72,18 +72,4 @@ public class TranscoderUtils {
 			return tConfigDao.findByName(DefaultsUtils.DEFAULT);
 		}
 	}
-	
-//	public static int getConfigId(String configName) throws MCASException {
-//		return loadConfig(configName).getId();
-//	}
-	
-//	public static String getInputWorkingDir(String configName) throws MCASException {
-//		DAO<TranscoderConfig> tConfigDao = new DAO<TranscoderConfig>(TranscoderConfig.class);
-//		try {
-//			return tConfigDao.findByName(configName).getInputWorkingDir();
-//		} catch (Exception e){
-//			e.printStackTrace();
-//			return tConfigDao.findByName(DefaultsUtils.DEFAULT).getInputWorkingDir();
-//		}
-//	}
 }
