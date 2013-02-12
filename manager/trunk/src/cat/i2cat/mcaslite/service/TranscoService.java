255fd6ef829b1b54954c7b6509b463f70ff7bd9c
Marc <marc@marc-i2cat.(none)>
2013-02-12 10:59:02 +0100

    ConfigReader added

--- manager/trunk/src/cat/i2cat/mcaslite/service/TranscoService.java
+++ manager/trunk/src/cat/i2cat/mcaslite/service/TranscoService.java
@@ -1,7 +1,15 @@
 package cat.i2cat.mcaslite.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+
+import org.apache.commons.io.FilenameUtils;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
 import cat.i2cat.mcaslite.cloud.CloudManager;
 import cat.i2cat.mcaslite.config.model.TRequest;
@@ -30,7 +38,40 @@ public class TranscoService {
 		clientTh.setName("MainManager");
 		clientTh.setDaemon(true);
 		clientTh.start();
-	
+		public class ConfigReader {
+			
+			private static String inputFile = FilenameUtils.concat(System.getProperty("mcas.home"),"META-INF/config.xml");
+			
+			public static String configGetter(String configAttribute){
+				
+			String returnItem = null;
+				
+			try { 
+					SAXBuilder builder = new SAXBuilder();
+					File xmlFile = new File(inputFile);
+			 
+					Document doc = (Document) builder.build(xmlFile);
+					
+					Element el = doc.getRootElement();
+					String[] children = configAttribute.split("\\.");
+					
+					for (String child : children){
+							el = el.getChild(child);
+					}
+					
+					returnItem =  el.getText();
+					
+				  } catch (IOException io) {
+					io.printStackTrace();
+				  } catch (JDOMException e) {
+					e.printStackTrace();
+				  }
+			
+			return returnItem;
+			
+			}
+		}
+		
 		
 	}
 }
