9e385f941e0e5c72bf9990208b26f5ff25110de8
Marc <marc@marc-i2cat.(none)>
2013-02-12 09:43:30 +0100

    Some changes

--- manager/trunk/src/cat/i2cat/mcaslite/service/TranscoService.java
+++ manager/trunk/src/cat/i2cat/mcaslite/service/TranscoService.java
@@ -2,20 +2,6 @@ package cat.i2cat.mcaslite.service;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import javax.servlet.ServletContext;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-import com.sun.jersey.spi.resource.Singleton;
 
 import cat.i2cat.mcaslite.cloud.CloudManager;
 import cat.i2cat.mcaslite.config.model.TRequest;
@@ -24,19 +10,15 @@ import cat.i2cat.mcaslite.management.Status;
 import cat.i2cat.mcaslite.management.TranscoHandler;
 import cat.i2cat.mcaslite.utils.RequestUtils;
 
-@Singleton
-@Path("/transco")
 public class TranscoService {
 	
-	@Context
-	private static ServletContext context;
+	private static TranscoHandler transcoH;
+	private static CloudManager client;
+	private static Thread managerTh;
+	private static Thread clientTh;
 	
-	private TranscoHandler transcoH;
-	private CloudManager client;
-	private Thread managerTh;
-	private Thread clientTh;
+	public static void main(String[] args){
 	
-	public TranscoService() {
 		transcoH = TranscoHandler.getInstance();
 		managerTh = new Thread(transcoH);
 		managerTh.setName("MainManager");
@@ -48,75 +30,7 @@ public class TranscoService {
 		clientTh.setName("MainManager");
 		clientTh.setDaemon(true);
 		clientTh.start();
-	}
 	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response addTransco(TRequest request) throws MCASException {
-		try {
-			if (!RequestUtils.isValidSrcUri(new URI(request.getSrc())) || !RequestUtils.isValidDestination(new URI(request.getDst()))) {
-				return Response.status(Response.Status.BAD_REQUEST).entity("Check source and destination.").build();
-			}
-		} catch (URISyntaxException e) {
-			return Response.status(Response.Status.BAD_REQUEST).entity("Check source and destination.").build();
-		}
-		if (transcoH.putRequest(request)){
-			return Response.status(Response.Status.CREATED).entity(request.getId()).build();
-		} else {
-			return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("System overloaded, wait and retry.").build();
-		}
-	}
-
-	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getStatus(@QueryParam("id") String id){
-		try {
-			Status status = transcoH.getStatus(id);
-			if (status == null){
-				throw new WebApplicationException(Response.Status.NOT_FOUND);
-			} else {
-				return status.toString();
-			}
-		} catch (MCASException e){
-			e.printStackTrace();
-			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
-	@GET
-	@Path("uris")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String getDestinationUris(@QueryParam("id") String id){
-		try {
-			TRequest request = transcoH.getRequest(id);
-			if (request == null){
-				throw new WebApplicationException(Response.Status.NOT_FOUND);
-			} else {
-				return RequestUtils.destinationJSONbuilder(request);
-			}
-		} catch (MCASException e) {
-			e.printStackTrace();
-			throw new WebApplicationException(Response.Status.INTERNAL_SERVER_ERROR);
-		}	
-	}
-	
-	@POST
-	@Path("cancel")
-	public Response cancelTransco(@QueryParam("id") String idStr, @DefaultValue("true") @QueryParam("interrupt") boolean interrupt) {
-		try {
-			TRequest request = transcoH.getRequest(idStr);
-			if (request == null){
-				return Response.status(Response.Status.NOT_FOUND).build();
-			} else {
-				if(transcoH.cancelRequest(request, interrupt)){
-					return Response.status(Response.Status.OK).build();
-				} else {
-					return Response.status(Response.Status.NOT_MODIFIED).build();
-				}
-			}
-		} catch (MCASException e) {
-			e.printStackTrace();
-			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-		}
+		
 	}
 }
