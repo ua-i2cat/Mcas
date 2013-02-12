08023f77d67fc4ca78084d091c93fc539757175c
Marc <marc@marc-i2cat.(none)>
2013-02-12 11:09:45 +0100

    ConfigReader2XMLReader

---
+++ manager/trunk/src/cat/i2cat/mcaslite/management/XMLReader.java
@@ -0,0 +1,53 @@
+package cat.i2cat.mcaslite.management;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+public class XMLReader {
+	
+	private static String inputFile;
+	
+	public XMLReader(String input){
+		this.inputFile = input;
+	}
+		
+	// FilenameUtils.concat(System.getProperty("mcas.home"),"META-INF/config.xml");
+		
+	public static String configGetter(String configAttribute){
+			
+	String returnItem = null;
+			
+	try { 
+			SAXBuilder builder = new SAXBuilder();
+			File xmlFile = new File(inputFile);
+	 
+			Document doc = (Document) builder.build(xmlFile);
+			
+			Element el = doc.getRootElement();
+			String[] children = configAttribute.split("\\.");
+			
+			for (String child : children){
+					el = el.getChild(child);
+			}
+			
+			returnItem =  el.getText();
+				
+		  } catch (IOException io) {
+			io.printStackTrace();
+		  } catch (JDOMException e) {
+			e.printStackTrace();
+		  }
+		
+	return returnItem;
+		
+	}
+	
+	
+}
+	
+	
